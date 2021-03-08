80e5752758f972a95e9eccf30157b849575239c1
Grubicio <wiktor.ziaja1@gmail.com>
2021-03-08 09:46:18 +0100

    Wszystkie ukonczone zadania

---
+++ navbar.js
@@ -0,0 +1,7 @@
+var nav = '<nav><ul>' +
+'<li><a href="index.html">Home</a></li>'+
+'<li><a href="contact.html">Kontakt</a></li>'+
+'</ul></nav>';
+
+var elem = document.querySelector(".navbar");
+elem.innerHTML = nav;
